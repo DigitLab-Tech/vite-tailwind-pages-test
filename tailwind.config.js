b2a68294fa8e30eef72b62e14f7ef3049beed887
Cedrick Plante <cedrick.plante@gmail.com>
2024-06-30 13:10:07 -0400

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["PT Sans"],
+      display: ["Raleway"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
