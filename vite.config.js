b77c8287ae4fbd4a40953cb748c1e078adeca8d8
Cedrick Plante <cedrick.plante@gmail.com>
2024-06-30 16:33:47 -0400

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { resolve } from "path";
 
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
+
 export default {
-  root: resolve(__dirname, "src"),
+  root,
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        products: resolve(root, 'products.html')
+      }
+    }
+  }
 };
